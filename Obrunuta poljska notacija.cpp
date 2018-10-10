acd69e771630100d0639c509bdc6d031263ff3f6
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-10-10 21:20:18 +0200

    Add files via upload

---
+++ Obrunuta poljska notacija.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+// Prototip funkcije
+bool daLiJeOperator(const string&);
+// Prototip funkcije
+void izvodjenjeOperatora(const string& , stack<double>&);
+
+int main(void)
+{
+    stack <double> stek; // Stek
+    string unos;
+
+    cout << "Kalkulator: Obrnuta poljska notacaija" << endl;
+
+    while(true) // Beskonacna petlja
+    {
+        // Prikaz zahteva
+        cout << ">> ";
+
+        // Uzimanje unosa
+        cin >> unos;
+
+        // Provera da li je broj
+        double broj;
+
+        if( istringstream(unos) >> broj ) // Kreiranje novog objekta
+        {
+            stek.push(broj); // Ubacivanje u stek
+        }
+        // Provera da li je operator
+        else if( daLiJeOperator(unos) )
+        {
+            izvodjenjeOperatora(unos, stek);
+        }
+        // Prekid rada programa
+        else if( unos == "q" )
+        {
+            return 0;
+        }
+        // Pogresan unos (NEVAZEÆI UNOS)
+        else
+        {
+            cout << endl << "Unos nije vazeci!" << endl;
+        }
+    }
+
+    return 0;
+}
+
+bool daLiJeOperator(const string& unos)
+{
+    string operatori[] = {"-", "+", "*", "/"};
+
+    for(int i = 0; i < 4; i++)
+    {
+        if( unos == operatori[i] )
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void izvodjenjeOperatora(const string& unos, stack<double>& stek)
+{
+    double vrednostLevo, vrednostDesno, rezultat;
+
+    vrednostDesno = stek.top();
+    stek.pop();
+
+    vrednostLevo = stek.top();
+    stek.pop();
+
+    if( unos == "-" )
+    {
+        rezultat = vrednostLevo - vrednostDesno;
+    }
+    else if( unos == "+" )
+    {
+        rezultat = vrednostLevo + vrednostDesno;
+    }
+    else if( unos == "*" )
+    {
+        rezultat = vrednostLevo * vrednostDesno;
+    }
+    else
+    {
+        rezultat = vrednostLevo / vrednostDesno;
+    }
+
+    cout << "\tRezultat: " << rezultat << endl;
+    stek.push(rezultat);
+
+    return;
+}
