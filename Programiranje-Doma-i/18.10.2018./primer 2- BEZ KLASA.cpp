89e88f5c9b44dc3ecf69268c1acf78232fb07e32
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-10-18 20:10:16 +0200

    Create primer 2- BEZ KLASA.cpp

---
+++ Programiranje-Doma-i/18.10.2018./primer 2- BEZ KLASA.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int najmanjiOdTriBroja(int b1, int b2, int b3)
+{
+    int m;
+
+    return ( m = (b1 < b2) ? b1 : b2 ) < b3 ? m : b3;
+}
+
+int main(void)
+{
+    int b1, b2, b3;
+
+    cout << "Unesite 1. broj: ";
+    cin >> b1;
+
+    cout << "Unesite 2. broj: ";
+    cin >> b2;
+
+    cout << "Unesite 3. broj: ";
+    cin >> b3;
+
+    cout << endl << "Najmanji broj je: " << najmanjiOdTriBroja(b1, b2, b3);
+
+    return 0;
+}
