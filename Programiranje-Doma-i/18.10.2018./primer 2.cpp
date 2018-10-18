d8c75e3395b0c852b8f670297c6b0b5ad20e9515
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-10-18 20:01:33 +0200

    Create primer 2.cpp

---
+++ Programiranje-Doma-i/18.10.2018./primer 2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+class Broj {
+    private:
+        int broj1, broj2, broj3;
+    public:
+        void unosBrojeva()
+        {
+            int b1, b2, b3;
+
+            cout << "Unesite 1. broj: ";
+            cin >> b1;
+            broj1 = b1;
+
+            cout << "Unesite 2. broj: ";
+            cin >> b2;
+            broj2 = b2;
+
+            cout << "Unesite 3. broj: ";
+            cin >> b3;
+            broj3 = b3;
+
+            return;
+        }
+        int najveciOdTriBrojaV1()
+        {
+            int b1 = broj1, b2 = broj2, b3 = broj3;
+
+            int minimum;
+
+            if(b1 < b2)
+                minimum = b1;
+            else
+                minimum = b2;
+
+            if(b3 < minimum)
+                minimum = b3;
+
+            return minimum;
+        }
+        int najveciOdTriBrojaV2()
+        {
+            int b1 = broj1, b2 = broj2, b3 = broj3;
+            int m;
+
+            return ( m = (b1 < b2) ? b1 : b2 ) < b3 ? m : b3;
+        }
+
+};
+
+int main(void)
+{
+    Broj b;
+
+    b.unosBrojeva();
+
+    cout << endl << "Najmanji broj je: " << b.najveciOdTriBrojaV1();
+    cout << endl << "Najmanji broj je: " << b.najveciOdTriBrojaV2();
+
+    return 0;
+}
