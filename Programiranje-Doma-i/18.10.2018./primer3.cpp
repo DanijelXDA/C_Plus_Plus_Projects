fa52387fe91faf7b69cb58fd4fd49767e0db824c
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-10-18 20:29:23 +0200

    Remove std::

--- Programiranje-Doma-i/18.10.2018./primer3.cpp
+++ Programiranje-Doma-i/18.10.2018./primer3.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+using namespace std;
 
 void unosDvaBroja(int& b1, int& b2)
 {
-    std::cout << "Unesite prvi broj: ";
-    std::cin >> b1;
+    cout << "Unesite prvi broj: ";
+    cin >> b1;
 
-    std::cout << "Unesite drugi broj: ";
-    std::cin >> b2;
+    cout << "Unesite drugi broj: ";
+    cin >> b2;
 
     return;
 }
@@ -22,7 +23,7 @@ int main(void)
     int b1, b2;
 
     unosDvaBroja(b1, b2);
-    std::cout << std::endl << "Zbir brojeva " << b1 << " + " << b2 << " je: " << saberi(b1, b2);
+    cout << endl << "Zbir brojeva " << b1 << " + " << b2 << " je: " << saberi(b1, b2);
 
     return 0;
 }
