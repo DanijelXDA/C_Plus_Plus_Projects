706517d1ef5d929259dcdd20d6a9145390f4f819
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-11-03 17:58:32 +0100

    Add ifstream example

---
+++ C++ Projekti/Čitanje iz datoteke.cpp
@@ -0,0 +1,30 @@
+﻿//////////////////////////////////////////////////////////////////
+//          PROGRAM KOJI CITA SADRZAJ JEDNE TEKSTUALNE DATOTEKE //
+//                  NAPOMENA: Datoteka mora postojati!          //
+//                  Cita se samo jedna linija teksta!          //
+/////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(void)
+{
+    string linijaTekstaIzDatoteke;
+    ifstream datoteka ("datoteka.txt"); // Otvaranje datoteke u modu citanja
+
+    if( datoteka.is_open() )
+    {
+        getline(datoteka, linijaTekstaIzDatoteke);
+
+        datoteka.close(); // Zatvaranje datoteènog toka
+
+    }
+    else
+        cout << "Datoteka ne postoji!" << endl;
+
+    cout << endl << linijaTekstaIzDatoteke << endl;
+
+    return 0;
+}
