6aec499e57c5fb98d828e8af117805e526a132cf
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2020-01-13 21:52:44 +0100

    Clean up

--- ETF ZBIRKA LASLO KRAUS ZADACI/Zadatak 5.10/main.cpp
+++ ETF ZBIRKA LASLO KRAUS ZADACI/Zadatak 5.10/main.cpp
@@ -5,48 +5,7 @@ using namespace std;
 
 #define MAX_ISPITA 20
 
-class Student : public Osoba
-{
-    private:
-        float ocene;
-        int polozeno;
-    public:
-        Student() : Osoba()
-        {
-            polozeno = 1;
-            ocene = 0.0;
-        }
-        Student(string i, int g) : Osoba(i, g)
-        {
-            polozeno = 1;
-            ocene = 0.0;
-        }
-        Student& operator+=(const float oc)
-        {
-            if(polozeno <= MAX_ISPITA) {
-                this -> ocene = ( this -> ocene + oc);
-                polozeno++;
-            }
-            else {
-                cout << endl << "POPUNJENA MESTA ZA SVE OCENE! (" << MAX_ISPITA << ")" << endl;
-            }
 
-            return *this;
-        }
-        int slobodnoOcena()
-        {
-            return (MAX_ISPITA - polozeno);
-        }
-        float avgOcene() { return ocene / (polozeno - 1); }
-
-        friend ostream &operator<<(ostream &out, Student &s)
-        {
-            /// ISPIS
-            cout << s.getIme() << " (" << s.getGodine() << ") / " << s.avgOcene() << endl;
-
-            return out;
-        }
-};
 
 class Imenik : Osoba
 {
